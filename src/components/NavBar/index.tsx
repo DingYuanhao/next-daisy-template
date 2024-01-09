ebf735c2b978a380419e3f66dc3d0e9aa7c8d54a
Ding Yuanhao <dingyuanhao14@126.com>
2024-01-09 10:39:41 +0800

    feat: added usehooks-ts

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,24 +1,22 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function NavBar() {
   const router = useRouter();
+
   return (
     <nav className="flex flex-col h-screen w-40 p-4 gap-4 bg-base-200">
       <button className="btn btn-ghost" onClick={() => router.push("/")}>
         Home
       </button>
-      <button className="btn btn-ghost" onClick={() => router.push("/dashboard")}>
+      <button
+        className="btn btn-ghost"
+        onClick={() => router.push("/dashboard")}
+      >
         DashBoard
       </button>
-      {/* <button className="btn mt-auto">
-        light / dark
-      </button> */}
-      <button className="btn btn-ghost mt-auto">
-        settings
-      </button>
+      <button className="btn btn-ghost">settings</button>
     </nav>
   );
 }
